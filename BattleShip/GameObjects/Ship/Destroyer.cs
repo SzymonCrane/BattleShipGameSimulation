b05612a1568e8c5d12cec209935cd13a0a54e06b
Szymon Żuraw <szymon.zurawit@wp.pl>
2021-11-07 21:35:12 +0100

    Added classes for each ship

---
+++ BattleShip/GameObjects/Ship/Destroyer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.GameObjects.Ship
+{
+    public class Destroyer : Ship
+    {
+        public Destroyer()
+        {
+            Name = "Destroyer";
+            Width = 3;
+            MarkType = MarkType.Destroyer;
+        }
+    }
+}
---
+++ BattleShip/GameObjects/Ship/Patrol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.GameObjects.Ship
+{
+    public class Patrol : Ship
+    {
+        public Patrol()
+        {
+            Name = "Patrol Boat";
+            Width = 2;
+            MarkType = MarkType.PatrolBoat;
+        }
+    }
+}
