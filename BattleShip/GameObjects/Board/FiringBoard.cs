43cf674f6d13e91437d47b0ebd121c3d890c5f37
Szymon Żuraw <szymon.zurawit@wp.pl>
2021-11-07 21:49:18 +0100

    Created two gaming boards where GameBoard class keeps values of coordinates on board, and FiringBoard class will create random shots on gaming board, while rejecting shots into same coordinates.

---
+++ BattleShip/GameObjects/Board/FiringBoard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.GameObjects.Board
+{
+    public class FiringBoard : GameBoard
+    {
+        public List<Coordinates> GetOpenRandomSpaceOnBoard() { }
+        public List<Coordinates> GetNearHit() { }
+        public List<SpaceOnBoard> GetNearShip(Coordinates coordinates) { }
+    }
+}
---
+++ BattleShip/GameObjects/Board/GameBoard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.GameObjects.Board
+{
+    public class GameBoard
+    {
+        public List<SpaceOnBoard> SpaceOnBoards { get; set; }
+        
+        public GameBoard()
+        {
+            SpaceOnBoards = new List<SpaceOnBoard>();
+            for (var i = 1; i < 10; i++)
+            {
+                for (var j = 1; j < 10; j++)
+                {
+                    SpaceOnBoards.Add(new SpaceOnBoard(i, j));
+                }
+            }
+        }
+    }
+}
