b987caec8c1beb3f448243081e50fb9e974db9bb
Szymon Żuraw <szymon.zurawit@wp.pl>
2021-11-07 21:20:24 +0100

    Dodaj pliki projektów.

---
+++ BattleShip/Enumeration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    public enum MarkType
+    {
+        // Assign a name of battleship to it's easier recognition on board - using 2002 naming version created by Hasbro
+
+        [Description("o")]
+        Empty,
+
+        [Description("C")]
+        Carrier,
+
+        [Description("B")]
+        Battleship,
+
+        [Description("D")]
+        Destroyer,
+
+        [Description("S")]
+        Submarine,
+
+        [Description("P")]
+        PatrolBoat,
+
+        [Description("X")]
+        Hit,
+
+        [Description("M")]
+        Miss,
+    }
+    public enum ShotResult
+    {
+        Miss,
+        Hit
+    }
+}
---
+++ BattleShip/GameObjects/Board/SpaceOnBoard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.GameObjects.Board
+{
+    public class SpaceOnBoard
+    {
+        public MarkType MarkType { get; set; }
+        public Coordinates Coordinates { get; set; }
+        public SpaceOnBoard(int row, int column)
+        {
+            Coordinates = new Coordinates(row, column);
+            MarkType = MarkType.Empty;
+        }
+        //public string Status
+        //{
+        //    get
+        //    {
+        //        return MarkType.GetNames<DescriptionAttribute>().Description;
+        //    }
+        //}
+        public bool IsTaken
+        {
+            get
+            {
+                return MarkType == MarkType.Carrier
+                    || MarkType == MarkType.Battleship
+                    || MarkType == MarkType.Destroyer
+                    || MarkType == MarkType.Submarine
+                    || MarkType == MarkType.PatrolBoat;
+            }
+        }
+        public bool IsRandomCoordinateAvailable
+        {
+            get
+            {
+                return (Coordinates.Row % 2 == 0 && Coordinates.Column % 2 == 0) || (Coordinates.Row % 2 == 1 && Coordinates.Column % 1 == 0);
+            }
+        }
+    }
+}
